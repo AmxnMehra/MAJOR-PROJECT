3bfe4bc5811fc8a9ac25f64773e5746f19a9a53e
AmxnMehra <amanmehra0112233@gmail.com>
2024-09-05 20:54:31 +0530

    Re-Style Rating

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,4 +1,4 @@
-const Listing = require("../routes/listing");
+const Listing = require("../models/listing");
 
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
