be89ffe01ee52876197a5ace9abc17b0078ec286
AmxnMehra <amanmehra0112233@gmail.com>
2024-10-10 11:32:11 +0530

    Map Marker

--- wanderlust/public/js/map.js
+++ wanderlust/public/js/map.js
@@ -2,6 +2,10 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: "map", //container id
   style: "mapbox://style/mapbox/streets-v12",
-  center: [77.209, 28.6139], //starting position [lng, lat]
+  center: coordinates, //starting position [lng, lat]
   zoom: 9,
 });
+
+console.log(coordinates);
+
+const marker = new mapboxgl.Marker().setLngLat(coordinates).addTo(map);
